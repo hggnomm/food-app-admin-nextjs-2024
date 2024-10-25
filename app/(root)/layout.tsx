30ce7cd6dc54a639a7a114c696e7280e264ca066
hggnomm <hggnomm.dev@gmail.com>
2024-10-25 22:52:29 +0700

    Root layout file for interface children ReactNode

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,8 @@
+interface SetupLayoutProps {
+  children: React.ReactNode;
+}
+const SetupLayout = ({ children }: SetupLayoutProps) => {
+  return <div>{children}</div>;
+};
+
+export default SetupLayout;
